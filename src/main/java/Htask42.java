e0db21414b63542b3cf6479a95e665006f5c56e4
Stepuro Mikhail <s1t2e3p4@mail.ru>
2023-03-11 12:24:55 +0300

    Записал решение задачи 2 в Htask42.java

--- src/main/java/Htask42.java
+++ src/main/java/Htask42.java
@@ -34,5 +34,38 @@
  * s consists of parentheses only '()[]{}'.
  */
 
+import java.util.*;
+
 public class Htask42 {
+    public static void main(String[] args) {
+        String symbols = "({[]})";
+        System.out.println(etoValidno(symbols));
+}
+    public static Boolean etoValidno(String text) {
+        Boolean flag = false;
+        Deque<String> list = new LinkedList<>();
+        String[] arr = new String[text.length()];
+        for (int i = 0; i < text.length(); i++) {
+            arr[i] = (text.substring(i, i + 1));
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals("(") || arr[i].equals("{") || arr[i].equals("[")) {
+                list.addFirst(arr[i]);
+            } else {
+                if (!list.isEmpty()
+                        && (list.peekFirst().equals("(") && arr[i].equals(")"))
+                        || (list.peekFirst().equals("{") && arr[i].equals("}"))
+                        || (list.peekFirst().equals("[") && arr[i].equals("]"))) {
+                    list.pop();
+                } else {
+                    return flag;
+                }
+            }
+        }
+        if (list.isEmpty()) {
+            flag = true;
+        }
+        return flag;
+    }
 }
