8b2dcf852a182be4c99c5a41c3b8c5fa67b96c8e
Stepuro Mikhail <s1t2e3p4@mail.ru>
2023-03-11 12:10:04 +0300

    Записал решение задачи Htask41

--- src/main/java/Htask41.java
+++ src/main/java/Htask41.java
@@ -7,5 +7,27 @@
  * 4->3->2->1
  */
 
+import java.util.*;
+
 public class Htask41 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число, равное колличеству элементов Вашего списка, и нажмите enter: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Введите число и нажмите enter: ");
+        Deque<Integer> list = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            list.addLast(scanner.nextInt());
+        }
+        scanner.close();
+
+        Deque<Integer> reversedlist = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            reversedlist.addFirst(list.pop());
+        }
+
+        System.out.println(reversedlist);
+
+    }
 }
